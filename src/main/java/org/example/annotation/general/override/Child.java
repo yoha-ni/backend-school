82836108c8b5503d18f053d41df13cbb12e60c19
yoha-ni <yohan7913@naver.com>
2025-03-18 17:17:38 +0900

    [Annotation, Reflection] General Annotation: Override

---
+++ src/main/java/org/example/annotation/general/override/Child.java
@@ -0,0 +1,9 @@
+package org.example.annotation.general.override;
+
+public class Child extends Parent {
+
+    @Override
+    public void printMessage() {
+        System.out.println("I'm child class");
+    }
+}
---
+++ src/main/java/org/example/annotation/general/override/Parent.java
@@ -0,0 +1,11 @@
+package org.example.annotation.general.override;
+
+public class Parent {
+    public void printMessageNotOverride() {
+        System.out.println("I'm parent class [not override]");
+    }
+
+    public void printMessage(){
+        System.out.println("I'm parent class");
+    }
+}
